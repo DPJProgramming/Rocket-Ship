fd6010687c49c9e1f09bd9761a49bcc75ecf46fe
cheesy d <cheesyd64@hotmail.com>
2023-01-09 16:47:40 -0800

    Created print nose cone method

--- Rocket Ship/Program.cs
+++ Rocket Ship/Program.cs
@@ -1,1 +1,31 @@
 ﻿
+printNoseCone();
+
+
+void printNoseCone() {
+
+	for (int line = 1; line <= (5); line++) {
+
+		//this will print out spaces
+		for (int i = 1; i <= (-1 * line + (10)); i++) {
+			Console.Write(" ");
+		}
+
+		//this will print out forward slashes
+		for (int i = 1; i <= line; i++) {
+			Console.Write("/");
+		}
+
+		//this will print the 2 stars in the center of cone
+		Console.Write("**");
+
+		//this will print the backslashes
+		for (int i = 1; i <= line; i++) {
+			Console.Write("\\");
+		}
+		Console.WriteLine("");
+	}
+}
+
+
+
