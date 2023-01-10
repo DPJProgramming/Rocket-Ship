c5f6088db9565f8282a452cb4b12e78920557cf1
cheesy d <cheesyd64@hotmail.com>
2023-01-09 16:54:14 -0800

    Created printBody method and printDiamondTop method

--- Rocket Ship/Program.cs
+++ Rocket Ship/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 printNoseCone();
+printBody();
 
 
 void printNoseCone() {
@@ -26,6 +27,109 @@ void printNoseCone() {
 		Console.WriteLine("");
 	}
 }
+
+static void printBody() {
+	printEdge();
+	printDiamondTop();
+	printDiamondBottom();
+	printEdge();
+	printDiamondBottom();
+	printDiamondTop();
+
+	printEdge();
+}
+
+static void printDiamondTop() {
+
+	for (int line = 1; (line <= 10); line++) {
+
+		//print "|" for every line
+		Console.Write("|");
+
+		//prints left "."
+		for (int i = 1; i <= (10 - line); i++) {
+			Console.Write(".");
+		}
+
+		//prints left "/\"
+		for (int i = 1; i <= (line); i++) {
+			Console.Write("/\\");
+		}
+
+		//prints middle "."
+		for (int i = 1; i <= -2 * line + 2 * 10; i++) {
+			Console.Write(".");
+		}
+
+		//prints right "/\"
+		for (int i = 1; i <= (line); i++) {
+			Console.Write("/\\");
+		}
+
+		//prints right "."
+		for (int i = 1; i <= (10 - line); i++) {
+			Console.Write(".");
+		}
+
+		//print "|" for every line on right side
+		Console.WriteLine("|");
+	}
+}
+
+//this will print the middle of the body
+static void printDiamondBottom() {
+
+	for (int line = 1; (line <= 10); line++) {
+
+		//print "|" for every line
+		Console.Write("|");
+
+		//prints left dots
+		for (int i = 1; i <= (line - 1); i++) {
+			Console.Write(".");
+		}
+
+		//prints "\/"
+		for (int i = 1; i >= (line - 10 + 1); i--) {
+			Console.Write("\\/");
+		}
+
+		//prints middle dots
+		for (int i = 1; i <= (2 * line - 2); i++) {
+			Console.Write(".");
+		}
+
+		//prints "\/"
+		for (int i = 1; i >= (line - 10 + 1); i--) {
+			Console.Write("\\/");
+		}
+
+		//prints right "."
+		for (int i = 1; i <= (line - 1); i++) {
+			Console.Write(".");
+		}
+
+		//print "|" for every line on right
+		Console.WriteLine("|");
+
+	}
+}
+
+static void printEdge() {
+
+	//this will print out + on left
+	Console.Write("+");
+
+	//this prints *=
+	for (int line = 1; line <= (2 * 10); line++) {
+		Console.Write("=*");
+	}
+
+
+	Console.WriteLine("+");
+}                
+
+
 
 
 
